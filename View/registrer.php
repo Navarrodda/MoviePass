3aecf9e375995cc82d9cc30e8176b40e2f137c04
Navarrodda <david95el@hotmail.com>
2019-09-25 15:48:53 -0300

    view modification register

--- View/registrer.php
+++ View/registrer.php
@@ -16,10 +16,11 @@
               </div>
               <div class="form">
                 <form class="login-form">
-                    <input type="text" placeholder="User Name"/>
+                    <input type="text" placeholder="First Name and Last Name"/>
                     <input type="text" placeholder="Nikname"/>
                     <input type="email" placeholder="Email"/>     
-                    <input type="password" placeholder="Password"/>        
+                    <input type="password" placeholder="Password"/>
+                    <input type="password" placeholder="Confirm password"/>          
                     <input class="subbt" type="submit" value="Sign In" style="border:none;"/>   
                 </form>
                 <input type="checkbox" id="remember" name="remember"><span style="color:#b6b6b6;font-size: 0.9em;"> Remember Me</span><a href="#">Forgot your password?</a>        
