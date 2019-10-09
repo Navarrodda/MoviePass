8ca8539ed993ef64e57909f776b0816fc9968d7d
gungamerr <nolosonie1@gmail.com>
2019-10-08 21:23:39 -0300

    delete label test

--- View/navbar.php
+++ View/navbar.php
@@ -43,7 +43,6 @@
       <div class="col-md-7">
         <ul class="nav navbar-nav">
           <li class="active"><a href="<?php echo URL ?>">HOME</a></li>
-          <li class="active"><a href="<?php echo URL ?>/movie/getAllPages/">TEST</a></li>
           <?php //if(!empty($_SESSION["rol"])){ ?>
             <?php //if($_SESSION["rol"] != 3){ ?>
               <li><a href="<?php echo URL?>/view/movies/">MOVIES</a></li>
