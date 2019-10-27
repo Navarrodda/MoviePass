a15a243d9ec7b9256dc28d7582732297d24ab5c4
Navarrodda <david95el@hotmail.com>
2019-10-26 21:24:18 -0300

    Modification in controler view

--- Controllers/ViewController.php
+++ Controllers/ViewController.php
@@ -94,19 +94,19 @@ class ViewController
 		$values = $this->ControlMovies->getList(1);
 		$value = $this->ControlMovies->bringmovies();
 		$i = 0;
-			foreach ($values as $data) {
-				
-				if($this->ControlMovies->bring_id_by_idapi($data->getIdapi()))
-				{
-					$values[$i]->codigo = TRUE;
-	
-				}
-				else
-				{
-					$values[$i]->codigo = FALSE;
-				}
-				$i++;
+		foreach ($values as $data) {
+
+			if($this->ControlMovies->bring_id_by_idapi($data->getIdapi()))
+			{
+				$values[$i]->codigo = TRUE;
+
 			}
+			else
+			{
+				$values[$i]->codigo = FALSE;
+			}
+			$i++;
+		}
 		$genere = $this->ControlGenre->getList();
 		$length = $this->ControlMovies->getAllPages();
 		include URL_VISTA . 'header.php';
@@ -120,13 +120,27 @@ class ViewController
 		$values = NULL;
 		$values = $this->ControlMovies->getMovieByGenre($id);
 		$genere = $this->ControlGenre->getList();
-		$length = count($values);
 
 		$strip = array("~", "`", "!", "@", "#", "#", "$", "%", "^", "&", "*", "(", ")", "_", "=", "+", "[", "{", "]",
 			"}", "\\", "|", ";", ":", "\"", "'", "&#;", "&#;", "3", "4","5","6","7","8","9","10","9", "â€”", "â€“", ",", "<", ".", ">", "/", "?","20");
 		$genre = trim(str_replace($strip, " ", strip_tags($genre)));
 		$genre = preg_replace('/\s+/', " ", $genre);
 		$titule = ucwords($genre);	
+
+		$i = 0;
+		foreach ($values as $data) {
+
+			if($this->ControlMovies->bring_id_by_idapi($data->getIdapi()))
+			{
+				$values[$i]->codigo = TRUE;
+
+			}
+			else
+			{
+				$values[$i]->codigo = FALSE;
+			}
+			$i++;
+		}
 		include URL_VISTA . 'header.php';
 		require(URL_VISTA . "genre.php");
 		include URL_VISTA . 'footer.php';
@@ -172,6 +186,20 @@ class ViewController
 		$value = $this->ControlMovies->bringmovies();
 		$values = $this->ControlMovies->getList($page);
 		$genere = $this->ControlGenre->getList();
+				$i = 0;
+		foreach ($values as $data) {
+
+			if($this->ControlMovies->bring_id_by_idapi($data->getIdapi()))
+			{
+				$values[$i]->codigo = TRUE;
+
+			}
+			else
+			{
+				$values[$i]->codigo = FALSE;
+			}
+			$i++;
+		}
 		include URL_VISTA . 'header.php';
 		require(URL_VISTA . "movies.php");
 		include URL_VISTA . 'footer.php';
