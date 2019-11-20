62ad2d867cb26945ab0a1c0640e8c7d06ae474d6
Navarrodda <david95el@hotmail.com>
2019-11-19 21:05:30 -0300

    Modification in grafic

--- View/graphic.php
+++ View/graphic.php
@@ -6,9 +6,9 @@
   <?php if(isset($this->message)) {?>
     <div class="container">
       <h1> <?= $this->message->cartelAlert($this->message->getMessage(),$this->message->getTipo()) ?></h1>
-    </div>
-  <?php } ?>
-  <div class="clear"></div>
+  </div>
+<?php } ?>
+<div class="clear"></div>
 </section>
 
 <section>
@@ -20,8 +20,8 @@
         else{ ?>
           <h2 class="section-heading">No functions registered to date and time are <?= $current_date ?></h2>
           <hr class="primary"> <?php } ?>
-        </div>
-        <div class="container">
+      </div>
+      <div class="container">
           <div class="row">
             <div class="col-xs-12">
               <?php if(!empty($movies)){
@@ -29,49 +29,55 @@
                   <table class="table marc3">
 
                     <tbody> 
-                     <tr>
-                      <td rowspan="12" valign="middle" class="marctr3"><p><img class="tablesimg " src="<?= $mov->getPoster(); ?>"></p></td>
-                    </tr>
-                    <tr class="marctr1">
-                      <td colspan="2">Titile: <?= $mov->getTitle();?></td>
-                      <td colspan="1"class="fa fa-pied-piper-pp" > Popularity: <?= $mov->getPopularity();?></td>
-                      <td colspan="1" class="fa fa-comment"> Language: <?= $mov->getLanguage();?></td>
-                      <td colspan="1" class="fa fa-thumbs-o-up"> Votes: <?= $mov->getVote();?></td>
-                      <td colspan="1" class="fa fa-play-circle-o"> Duration: <?= $mov->getDuration();?></td>
-                    </tr>
-                    <tr style="color:white"> 
-                      <td colspan="12" class="marctr2">Overview: <?= $fun->getMovie()->getOverview();?></td>
-                    </tr>
-                    <?php if(!empty($roomcinema)) { 
-                     foreach ($roomcinema as $roomci){
-                      if($mov->getId() === $roomci->getMovie()->getId()) { ?>
-                        <tr rowspan="12" style="color:white">
-                          <td colspan="1">Cinema: <?= $roomci->getRoom()->getCinema()->getNombre();  ?></td>
-                          <td colspan="1">Day</td>
-                          <td colspan="1">Hours</td>
-                          <td colspan="1">Estimated Price</td>
-                          <td colspan="1">Total Income</td>
-                          <td colspan="1">Cant Buy</td>
-                          <td colspan="1">Vacancy</td>
-                          <td colspan="1">Cant Sites</td>
-                        </tr>
-                        <tr style="color:white">
-                         <td colspan="1">Room <?= $roomci->getRoom()->getNameRoom();?></td>
-                         <td colspan="1"><?php $fecha = date("d/m/Y", strtotime($roomci->getDia())); echo $fecha?></td>
-                         <td colspan="1"><?= $roomci->getHora(); ?></td>
-                         <td colspan="1"><?= $roomci->getRoom()->getCinema()->getValor_entrada(); ?></td>
-                         <td colspan="1"><?= $roomci->buy?></td>
-                         <td colspan="1"><?= $roomci->coun; ?></td>
-                         <td colspan="1"><?= $roomci->min; ?></td>
-                         <td colspan="1"><?= $roomci->getRoom()->getCantSite()?></td>
-
-                         </tr>
-                       <?php } } } ?>
-                     </tbody>
-                   </table>
-                 <?php }  } ?>
-               </div>
-             </div>
-           </div>
-         </div>
-       </section>
+                       <tr>
+                          <td rowspan="12" valign="middle" class="marctr3"><p><img class="tablesimg " src="<?= $mov->getPoster(); ?>"></p></td>
+                      </tr>
+                      <tr class="marctr1">
+                          <td colspan="2">Titile: <?= $mov->getTitle();?></td>
+                          <td colspan="1"class="fa fa-pied-piper-pp" > Popularity: <?= $mov->getPopularity();?></td>
+                          <td colspan="1" class="fa fa-comment"> Language: <?= $mov->getLanguage();?></td>
+                          <td colspan="1" class="fa fa-thumbs-o-up"> Votes: <?= $mov->getVote();?></td>
+                          <td colspan="1" class="fa fa-play-circle-o"> Duration: <?= $mov->getDuration();?></td>
+                      </tr>
+                      <tr style="color:white"> 
+                          <td colspan="12" class="marctr2">Overview: <?= $fun->getMovie()->getOverview();?></td>
+                      </tr>
+                      <?php if(!empty($roomcinema)) { 
+                       foreach ($roomcinema as $roomci){
+                          if($mov->getId() === $roomci->getMovie()->getId()) { ?>
+                            <tr rowspan="12" style="color:white">
+                              <td colspan="1">Cinema: <?= $roomci->getRoom()->getCinema()->getNombre();  ?></td>
+                              <td colspan="1">Day</td>
+                              <td colspan="1">Hours</td>
+                              <td colspan="1">Estimated Price</td>
+                              <td colspan="1">Total Income</td>
+                              <td colspan="1">Cant Buy</td>
+                              <td colspan="1">Vacancy</td>
+                              <td colspan="1">Cant Sites</td>
+                          </tr>
+                          <tr style="color:white">
+                           <td colspan="1">Room <?= $roomci->getRoom()->getNameRoom();?></td>
+                           <td colspan="1"><?php $fecha = date("d/m/Y", strtotime($roomci->getDia())); echo $fecha?></td>
+                           <td colspan="1"><?= $roomci->getHora(); ?></td>
+                           <td colspan="1"><?= $roomci->getRoom()->getCinema()->getValor_entrada(); ?></td>
+                           <?php if(!empty($roomci->buy)){ ?>
+                               <td colspan="1"><?= $roomci->buy?></td>
+                               <td colspan="1"><?= $roomci->coun; ?></td>
+                               <td colspan="1"><?= $roomci->min; ?></td>
+                               <td colspan="1"><?= $roomci->getRoom()->getCantSite()?></td>
+                           <?php } else { ?>
+                           <?php } ?>
+                           <td colspan="1">0</td>
+                           <td colspan="1">0</td>
+                           <td colspan="1">0</td>
+                           <td colspan="1"><?=$roomci->getRoom()->getCantSite()?></td>
+                       </tr>
+                   <?php } } } ?>
+               </tbody>
+           </table>
+       <?php }  } ?>
+   </div>
+</div>
+</div>
+</div>
+</section>
