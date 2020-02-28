77fa8df514d9a02260304e83a197739a7c6524f7
Navarrodda <davidddi.dn@gmail.com>
2020-02-28 17:26:55 -0300

    Modify view acount

--- View/account.php
+++ View/account.php
@@ -8,7 +8,11 @@
       <thead>
        <th style="color:white">Name</th>
        <th style="color:white">Lastname</th>
+       <?php if($user->getDni() != null){ ?>
        <th style="color:white">DNI</th>
+       <?php } else{ ?>
+        <th style="color:white">Account</th>
+        <?php  } ?>
        <th style="color:white">Nikname</th>
        <th style="color:white">Email</th>
      </thead>
@@ -17,17 +21,24 @@
          <tr>
           <td style="color:white"><?php echo $user->getName();?></td>
           <td style="color:white"><?php echo $user->getLastname(); ?></td>
+          <?php if($user->getDni() != null){ ?>
           <td style="color:white"><?php echo $user->getDni();?></td>
+          <?php } else{ ?>
+            <td style="color:white">FaceBook</td>
+          <?php  } ?>
           <td style="color:white"><?php echo $user->getNikname(); ?></td>
           <td style="color:white"><?php echo $user->getEmail();?></td>
+          <?php if($user->getDni() != null){?>
           <td> 
            <a class="btn btn-warning" href="<?php echo URL ?>/view/modifyaccount/">Modify</a> 
          </td>
+       <?php  } ?>
        </tr>
      </form>
    </tbody>
  </table>
- <?php if ($_SESSION["rol"] == 3) { ?>
+ <?php if ($_SESSION["rol"] == 3) { 
+    if($user->getDni() != null){ ?>
   <script type="text/javascript">
     function valida(dato) {
       var ok = true;
@@ -65,7 +76,7 @@
 </form>
 </tbody>
 </table>
-<?php } ?>
+<?php } } ?>
 </div>
 </div>
 </section>
